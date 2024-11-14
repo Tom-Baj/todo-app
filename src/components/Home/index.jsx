8cfba0b3100cafdb7c1b95bb67c652a74758553e
Tom-Baj <140359887+Tom-Baj@users.noreply.github.com>
2024-11-14 04:43:32 +0100

    update

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,16 +1,18 @@
-import styled from 'styled-components';
+/* import styled from 'styled-components';
+ */ import TodoTitle from '../../components/TodoTitle';
 import TodoItem from '../../components/TodoItem';
 
-const StyledTitre = styled.h1`
+/* const StyledTitre = styled.h1`
     color: #333;
     font-size: 2rem;
     text-align: center;
-`;
+`; */
 
 function Home() {
     return (
         <div className="main">
-            <StyledTitre>To-Do List</StyledTitre>
+            {/*             <StyledTitre>To-Do List</StyledTitre> */}{' '}
+            <TodoTitle />
             <TodoItem />
         </div>
     );
